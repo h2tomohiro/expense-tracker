99c96cd266b2db4282eacc77788f4d02ec61d12c
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-03-24 18:06:35 +0100

    to make it work

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,8 +1,25 @@
-import React, { useContext } from "react";
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
 
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
+
   const amounts = transactions.map(transaction => transaction.amount);
 
   const income = amounts
@@ -13,16 +30,17 @@ export const IncomeExpenses = () => {
     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   );
+
   return (
-    <div class="inc-exp-container">
-      <div>
-        <h4>Income</h4>
-        <p class="money plus">{income}</p>
+    <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">{moneyFormatter(income)}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">{moneyFormatter(expense)}</p>
+        </div>
       </div>
-      <div>
-        <h4>Expense</h4>
-        <p class="money plus">{expense}</p>
-      </div>
-    </div>
   )
 }
