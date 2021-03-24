6e1e771c1e1c1789ba2d406549bbca0aeed7ede1
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-03-23 18:44:32 -0700

    Add transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -8,9 +10,7 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<li className='minus'>
-          {transaction.text} <span>-$400</span><button class="delete-btn">x</button>
-        </li>))}
+        {transactions.map(transaction => (<Transaction transaction={transaction} />))}
       </ul>
     </>
   )
