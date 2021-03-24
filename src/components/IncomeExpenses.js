37338f1804a6323c4473baf510d5e799998e767a
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-03-23 19:22:14 -0700

    Delete History

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  );
   return (
     <div class="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p class="money plus">+0.00</p>
+        <p class="money plus">{income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p class="money plus">-0.00</p>
+        <p class="money plus">{expense}</p>
       </div>
     </div>
   )
