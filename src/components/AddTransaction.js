eebfffc6f4058657dfc4c8d88e44c51485b5a93a
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-03-24 19:13:03 -0700

    Delete unnecessary code

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState';
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
+// import DatePicker from "react-datepicker"
+// import "react-datepicker/dist/react-datepicker.css"
 
 export const AddTransaction = () => {
-  const [date, setStartDate] = useState(new Date())
+  const [date, setStartDate] = useState('')
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
@@ -33,22 +33,18 @@ export const AddTransaction = () => {
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="date">Date</label>
-          {/* <input type="text" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Chose date..." /> */}
-          {/* <DatePicker type="text" selected={startDate} onChange={handleChange} /> */}
-          {/* <DatePicker selected={date} value={date} onChange={(e) => setStartDate(e.target.value)} /> */}
           <input type="date" onChange={(e) => setStartDate(e.target.value)} required/>
-          {/* <DatePicker selected={date} value={date} onChange={date => setStartDate(date)} /> */}
         </div>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." required/>
         </div>
         <div className="form-control">
           <label htmlFor="amount"
           >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." required/>
         </div>
         <button className="btn">Add transaction</button>
       </form>
