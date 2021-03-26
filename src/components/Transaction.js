769c4e7af104d89c3b6febae19ec394869fd47da
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-03-25 17:58:21 -0700

    fix eslint

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,6 +5,7 @@ export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
+    // eslint-disable-next-line no-sequences
     <li className={"col-12", transaction.amount < 0 ? 'minus' : 'plus'} >
       <span>{transaction.date}</span>{transaction.text} <span> {sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} class="delete-btn">x</button>
     </li>
